333bbd9d1be66a22df74076e641060b9da9e87c4
Daniel Wojda <wojdadan@gmail.com>
2018-08-12 21:21:52 +0100

    Add support for Gauge metric
    Penstock must support all kinds of common metrics: counters, gauges, histograms. This commit generalize metric logic and introduce a second metric type - gauges.

--- src/main/scala/io/woodenmill/penstock/metrics/prometheus/PrometheusClient.scala
+++ src/main/scala/io/woodenmill/penstock/metrics/prometheus/PrometheusClient.scala
@@ -1,37 +1,40 @@
 package io.woodenmill.penstock.metrics.prometheus
 
 import java.net.URI
+
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.asynchttpclient.future.AsyncHttpClientFutureBackend
-import io.woodenmill.penstock.Metrics
+import io.woodenmill.penstock.Metric
 import io.woodenmill.penstock.Metrics._
+import io.woodenmill.penstock.metrics.prometheus.Prometheus.PromQl
+
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Success, Try}
 
 
 object Prometheus {
 
   case class PrometheusConfig(prometheusUrl: URI)
 
-  object PromQl {
-    def apply(queryToParse: String): Try[PromQl] = Success(new PromQl(queryToParse))
+  case class PromQl[M <: Metric[_]](query: String, metricFactory: MetricFactory[M]) {
+    override def toString: String = s"query=$query"
   }
 
-  case class PromQl(query: String)
 }
 
 
 case class PrometheusClient(config: Prometheus.PrometheusConfig) {
   private implicit val backend = AsyncHttpClientFutureBackend()
 
-  def fetch(query: Prometheus.PromQl)(implicit ec: ExecutionContext): Future[Metrics.Counter] = {
+  def fetch[B <: Metric[_]](query: PromQl[B])(implicit ec: ExecutionContext): Future[B] = {
     val promApi = Uri(config.prometheusUrl).path("/api/v1/query")
     val request = sttp.get(promApi.param("query", query.query))
+
     request.send().flatMap {
       case r @ Response(Right(body), code, _, _, _) if r.isSuccess =>
-        Future.fromTry( MetricExtractor.extract[Counter](body) )
+        val triedCounter = MetricExtractor.extract[B](body)(query.metricFactory)
+        Future.fromTry( triedCounter )
       case Response(Left(body), code, _, _, _) =>
-        Future.failed(new RuntimeException(s"Querying Prometheus has failed. Query: $query. Response: status=$code, body=${new String(body)}"))
+        Future.failed(new RuntimeException(s"Querying Prometheus has failed. $query. Response: status=$code, body=${new String(body)}"))
     }
   }
 }
