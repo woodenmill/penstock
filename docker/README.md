e7808b1dddb64a09f1c1c4cf0fb470aed236304a
Daniel Wojda <wojdadan@gmail.com>
2018-08-12 18:11:29 +0100

    Add docker compose configuration so developers can play with Penstock locally

---
+++ docker/README.md
@@ -0,0 +1,11 @@
+# Run locally
+
+This directory allows you test your specifications locally. In order to run Zookeeper, Kafka, Prometheus and Grafana, type following command:
+
+`docker-compose up -d`
+
+| Component  | Url            |
+|------------|----------------|
+| Grafana    | localhost:3000 |
+| Prometheus | localhost:9090 |
+| Kafka      | localhost:9092 |
--- src/test/scala/io/woodenmill/penstock/examples/GettingStartedSpec.scala
+++ src/test/scala/io/woodenmill/penstock/examples/GettingStartedSpec.scala
@@ -8,55 +8,35 @@ import io.woodenmill.penstock.Metrics.Counter
 import io.woodenmill.penstock.backends.kafka.KafkaBackend
 import io.woodenmill.penstock.metrics.prometheus.Prometheus.{PromQl, PrometheusConfig}
 import io.woodenmill.penstock.metrics.prometheus.PrometheusMetric
-import io.woodenmill.penstock.testutils.{Ports, PromResponses, PrometheusIntegratedSpec}
-import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.scalatest.{AsyncFlatSpec, Matchers}
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class GettingStartedSpec extends AsyncFlatSpec with Matchers with EmbeddedKafka with PrometheusIntegratedSpec {
+class GettingStartedSpec extends AsyncFlatSpec with Matchers  {
 
-  val topic: String = "examples"
-  val kafkaPort: Int = Ports.nextAvailablePort()
 
-  implicit val kafkaConfig: EmbeddedKafkaConfig = EmbeddedKafkaConfig(kafkaPort = kafkaPort, zooKeeperPort = Ports.nextAvailablePort())
-
-  implicit val kafkaBackend: KafkaBackend = KafkaBackend(s"localhost:$kafkaPort")
   implicit val system: ActorSystem = ActorSystem("Getting-Started")
   implicit val mat: ActorMaterializer = ActorMaterializer()
-  implicit val promConfig: PrometheusConfig = PrometheusConfig(new URI(s"localhost:$prometheusPort"))
-
-  override protected def beforeAll(): Unit = {
-    super.beforeAll()
-    EmbeddedKafka.start()(kafkaConfig)
-    createCustomTopic(topic)
-  }
+  implicit val kafkaBackend: KafkaBackend = KafkaBackend("localhost:9092")
+  implicit val promConfig: PrometheusConfig = PrometheusConfig(new URI("localhost:9090"))
 
 
-  "Getting Started" should "send messages to Kafka and use custom Prometheus metric to verify behaviour" in {
+  ignore should "send messages to Kafka and use custom Prometheus metric to verify behaviour" in {
     //given
-    configurePromStub("up", PromResponses.valid("1"))
+    val topic: String = "input"
     val testMessage = new ProducerRecord[Array[Byte], Array[Byte]](topic, "test message".getBytes)
-    val customMetric = PrometheusMetric[Counter](PromQl("up").get)
+    val kafkaTopicMessagesRate = PrometheusMetric[Counter](PromQl(s"""kafka_server_BrokerTopicMetrics_OneMinuteRate{name="MessagesInPerSec",topic="$topic"}""").get)
 
     //when
-    val loadFinished = LoadRunner(testMessage, duration = 3.seconds, throughput = 200).run()
+    val loadFinished = LoadRunner(testMessage, duration = 5.minutes, throughput = 200).run()
 
     //then
     loadFinished.map { _ =>
       kafkaBackend.metrics().recordErrorTotal.value shouldBe 0
-      kafkaBackend.metrics().recordSendTotal.value should be (600L +- 50L)
-      customMetric.value() shouldBe 1
+      kafkaBackend.metrics().recordSendTotal.value should be (60000L +- 1000L)
+      kafkaTopicMessagesRate.value() shouldBe 200L +- 20L
     }
-  }
-
-  override protected def afterAll(): Unit = {
-    Await.ready(system.terminate(), 5.seconds)
-    kafkaBackend.shutdown()
-    EmbeddedKafka.stop()
-    super.afterAll()
   }
 
 }
