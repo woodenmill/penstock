d7c2daeda7da6a84d479120fabc9a2f99cc8fbe2
Daniel Wojda <wojdadan@gmail.com>
2018-11-12 21:01:25 +0000

    Refactor ConsoleReport so testing is easy

--- src/main/scala/io/woodenmill/penstock/report/ConsoleReport.scala
+++ src/main/scala/io/woodenmill/penstock/report/ConsoleReport.scala
@@ -8,23 +8,33 @@ import io.woodenmill.penstock.Metric
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.{FiniteDuration, _}
 
+object ConsoleReport {
+
+  type Report = String
+
+  def buildReport(metricIOs: List[IO[Metric[_]]])(implicit ec: ExecutionContext): IO[Report] =
+    metricIOs
+      .map(io => io.attempt)
+      .parSequence
+      .map(throwablesAndMetrics => throwablesAndMetrics.separate)
+      .map { case (throwables, metrics) =>
+        val metricsReport = AsciiTableFormatter.format(metrics)
+        val errorReport = throwables.map(t => s"Error: ${t.getMessage}").mkString("\n")
+        s"""
+           |$metricsReport
+           |$errorReport
+        """.stripMargin
+      }
+}
 
 case class ConsoleReport(metricIos: IO[Metric[_]]*) {
 
   def runEvery(interval: FiniteDuration)(implicit system: ActorSystem, printer: Printer = ConsolePrinter()): Unit = {
     implicit val ec: ExecutionContext = system.dispatcher
-
-    val report = metricIos
-      .map(io => io.attempt)
-      .toList.parSequence
-      .map(throwablesAndMetrics => throwablesAndMetrics.separate)
-      .map { case (throwables, metrics) =>
-        printer.printLine(AsciiTableFormatter.format(metrics))
-        throwables.foreach(t => printer.printLine(s"Error: ${t.getMessage}"))
-      }
-
     system.scheduler.schedule(0.seconds, interval) {
-      report.unsafeRunSync()
+      ConsoleReport.buildReport(metricIos.toList)
+        .map(report => printer.printLine(report))
+        .unsafeRunSync()
     }
   }
 }
